1a95b8057d61220844ef372a70005b33f5271f2f
Nertigel <nertigel14475@gmail.com>
2020-01-22 23:58:15 +0200

    upload yeet

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # nObfuscator
  nObfuscator is an obfuscation tool for protecting/hiding your lua code
+
+# Make sure you have lua 5.3, if you don't then you can download it here: www.lua.org/ftp/lua-5.3.5.tar.gz
+# You can rename the files to whatever you like but please don't re-sell or remove credits, thank you!
+
+# Version 1
+ - Open "nertigel_obfuscator.lua"
+ - Change "local thing" 's content to the code that you want to obfuscate
+ - Make sure you have "nertigel_obfuscated.lua" file. 
+ - Run "lua nertigel_obfuscator.lua"
+ - Output will go to "nertigel_obfuscated.lua"
+ 
+# Credits
+ - https://glot.io/snippets/f1tt9okm5w
+ - https://github.com/Direnta/LuaSeel
---
+++ v1/nertigel_obfuscator.lua
@@ -0,0 +1,18 @@
+local thing = [[
+	print('nObf: replace this line with your code.')
+]]
+
+local jcm="\n\n --// Obfuscated with nObf by Nertigel \n\n"
+local jcfs={"ilInErTiGElilI","ilINertIgeL5391nErt","IlinErTiGeLilI","iElIinlTI5391ilI","nErtIgElwAshErE","nE5rtIgElwAshErE","nErtIgEl5391E","nE5391rtIgEl5391E"}
+local jcq="local "..jcfs[math.random(1,#jcfs)].." = (5*3-2/8+9*2/9+8*3)".." "
+local jcn1="function nErTiGeL_ilIilI("..jcfs[math.random(1,#jcfs)]..")"..jcq.."end".." "
+local jcn2="function nErtIgEL_ilIilI("..jcfs[math.random(1,#jcfs)]..")"..jcq.."end".." "
+local junkcode=jcm..jcn1..jcn2..jcq
+
+local encoded = thing:gsub(".", function(bb) return "\\" .. bb:byte() end) or thing .. "\""
+
+print('nObf: setting file...')
+file = io.open("nertigel_obfuscated.lua", "a")
+print('nObf: file set.')
+file:write(jcm..jcn1..jcq..'load("'..encoded..'")() '..jcq..jcn2..jcq)
+print('nObf: file written successfully.')
